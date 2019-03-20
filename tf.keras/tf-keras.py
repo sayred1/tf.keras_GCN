bf564ff8fe93ba7823bf2031a6d4f3770c04c55e
qzhu2017 <alecfans@gmail.com>
2019-03-20 10:51:45 -0700

    add other GCN models

--- tf.keras/tf-keras.py
+++ tf.keras/tf-keras.py
@@ -50,6 +50,164 @@ class GCN(layers.Layer):
         _X = get_skip_connection(_X, X)
         return self.activation(_X)
 
+class AGCN(layers.Layer):
+    """
+    An attention graph convolution model
+    """
+    def __init__(self, output_dim, activation, **kwargs):
+        self.output_dim = output_dim
+        self.activation = activation
+        super(AGCN, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        shape = tf.TensorShape((input_shape[0][2], self.output_dim))
+        shape = [int(shape[0]),int(shape[1])] # [50 , 32]
+       
+        # att. weight, conv. weight, and conv. bias calculated in K = 4 subspaces
+        self.conv_weight = []
+        self.conv_bias = []
+        self.attn_weight = [] 
+        for i in range(4):
+            attn_kernel = self.add_weight(name='attn-kernel'+str(i),
+                                          shape=[shape[1],shape[1]],
+                                          initializer='glorot_uniform',
+                                          trainable = True)
+            self.attn_weight.append(attn_kernel)
+
+            kernel = self.add_weight(name='kernel',
+                                          shape=shape,
+                                          initializer='glorot_uniform',
+                                          trainable=True)
+            self.conv_weight.append(kernel)
+
+            bias = self.add_weight(name='bias',
+                                        shape=[shape[1]],
+                                        initializer='glorot_uniform',
+                                        trainable=True)
+            self.conv_bias.append(bias)
+
+    def call(self, input):
+        X, A = input[0], input[1]
+        dim = self.conv_weight[0].get_shape()[1]
+        num_atoms = A.get_shape()[1]
+        X_total = [] 
+        A_total = []
+        for i in range(len(self.conv_weight)):                  # _X and A (attention coeff.) calculed in K = 4 subspaces  
+            _b = tf.reshape(tf.tile(self.conv_bias[i], [num_atoms]), [num_atoms, dim])
+            _h = tf.einsum('ijk,kl->ijl', X, self.conv_weight[i]) + _b
+            _A = attn_matrix(A, _h, self.attn_weight[i])        # calculates the attention coefficient (changes weights between atoms)
+            _h = tf.nn.relu(tf.matmul(_A, _h))
+            X_total.append(_h)
+            A_total.append(_A)
+        _X = tf.nn.relu(tf.reduce_mean(X_total, 0))             # activation of the mean of K = 4 updated feature calculations
+        _A = tf.reduce_mean(A_total, 0)                         # mean of K = 4 attention coefficients
+        _X = get_skip_connection(_X, X)                         # basic skip-connection
+        return self.activation(_X)
+
+class AGGCN(layers.Layer):
+    """
+    An attention gated graph convolution model
+    """
+    def __init__(self, output_dim, activation, **kwargs):
+        self.output_dim = output_dim
+        self.activation = activation
+        super(AGGCN, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        shape = tf.TensorShape((input_shape[0][2], self.output_dim))
+        shape = [int(shape[0]),int(shape[1])] # [50 , 32]
+       
+        # att. weight, conv. weight, and conv. bias calculated in K = 4 subspaces
+        self.conv_weight = []
+        self.conv_bias = []
+        self.attn_weight = [] 
+        for i in range(4):
+            attn_kernel = self.add_weight(name='attn-kernel'+str(i),
+                                          shape=[shape[1],shape[1]],
+                                          initializer='glorot_uniform',
+                                          trainable = True)
+            self.attn_weight.append(attn_kernel)
+
+            kernel = self.add_weight(name='kernel',
+                                          shape=shape,
+                                          initializer='glorot_uniform',
+                                          trainable=True)
+            self.conv_weight.append(kernel)
+
+            bias = self.add_weight(name='bias',
+                                        shape=[shape[1]],
+                                        initializer='glorot_uniform',
+                                        trainable=True)
+            self.conv_bias.append(bias)
+        self.gate_bias = self.add_weight(name = 'gate_bias',
+                                         shape = [shape[1]],
+                                         initializer='glorot_uniform',
+                                         dtype=tf.float64)
+
+    def call(self, input):
+        X, A = input[0], input[1]
+        dim = self.conv_weight[0].get_shape()[1]
+        num_atoms = A.get_shape()[1]
+        X_total = [] 
+        A_total = []
+        for i in range(len(self.conv_weight)):                  # _X and A (attention coeff.) calculed in K = 4 subspaces  
+            _b = tf.reshape(tf.tile(self.conv_bias[i], [num_atoms]), [num_atoms, dim])
+            _h = tf.einsum('ijk,kl->ijl', X, self.conv_weight[i]) + _b
+            _A = attn_matrix(A, _h, self.attn_weight[i])        # calculates the attention coefficient (changes weights between atoms)
+            _h = tf.nn.relu(tf.matmul(_A, _h))
+            X_total.append(_h)
+            A_total.append(_A)
+        _X = tf.nn.relu(tf.reduce_mean(X_total, 0))             # activation of the mean of K = 4 updated feature calculations
+        _A = tf.reduce_mean(A_total, 0)                         # mean of K = 4 attention coefficients
+        _X = self.activation(_X)
+        if( int(X.get_shape()[2]) != dim ):
+            X = tf.layers.dense(X, dim, use_bias=False)
+        coeff = get_gate_coeff(_X,X,dim,self.gate_bias)     # gate coefficient (determines if _X is updated from recent layers)
+        _X = tf.multiply(_X,coeff) + tf.multiply(X,1.0-coeff)
+        return self.activation(_X)
+
+
+class GGCN(layers.Layer):
+    """
+    A gated graph convolution model
+    """
+    def __init__(self, output_dim, activation, **kwargs):
+        self.output_dim = output_dim
+        self.activation = activation
+        super(GGCN, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        #print(input_shape[0][2])
+        shape = tf.TensorShape((input_shape[0][2], self.output_dim))
+        shape = [int(shape[0]),int(shape[1])] # [50 , 32]
+
+        self.kernel = self.add_weight(name='kernel',
+                                      shape=shape,
+                                      initializer='glorot_uniform',
+                                      trainable=True)
+
+        self.bias = self.add_weight(name='bias',
+                                    shape=[shape[1]],
+                                    initializer='glorot_uniform',
+                                    trainable=True)
+        self.gate_bias = self.add_weight(name = 'gate_bias',
+                                         shape = [shape[1]], 
+                                         initializer='glorot_uniform',
+                                         trainable=True)
+    def call(self, input):
+        X, A = input[0], input[1]
+        dim = self.kernel.get_shape()[1]
+        num_atoms = A.get_shape()[1]
+        _b = tf.reshape(tf.tile(self.bias, [num_atoms]), [num_atoms, dim])
+        _X = tf.einsum('ijk,kl->ijl', X, self.kernel) + _b
+        _X = tf.matmul(A, _X)
+        _X = self.activation(_X)
+        if( int(X.get_shape()[2]) != dim ):
+            X = tf.layers.dense(X, dim, use_bias=False)
+        coeff = get_gate_coeff(_X,X,dim,self.gate_bias)     # gate coefficient (determines if _X is updated from recent layers)
+        _X = tf.multiply(_X,coeff) + tf.multiply(X,1.0-coeff)
+        return self.activation(_X)
+
 class G2N(layers.Layer):
     """
     A layer to sum the node feature to preserve the permutation invariance
@@ -95,9 +253,16 @@ class mymodel(tf.keras.Model):
         self.model_name = model_name
         self.graph_layers = []
         self.mlp_layers = []
-        if model_name == 'GCN':
-            for i, hid in enumerate(graph_layers):
+        for i, hid in enumerate(graph_layers):
+            if model_name == 'GCN':
                 self.graph_layers.append(GCN(hid, name='GCN'+str(i), activation=tf.nn.relu)) 
+            elif model_name == 'GGCN':
+                self.graph_layers.append(GGCN(hid, name='GGCN'+str(i), activation=tf.nn.relu)) 
+            elif model_name == 'AGCN':
+                self.graph_layers.append(AGCN(hid, name='AGCN'+str(i), activation=tf.nn.relu)) 
+            elif model_name == 'AGGCN':
+                self.graph_layers.append(AGCN(hid, name='AGGCN'+str(i), activation=tf.nn.relu)) 
+
         self.g2n = G2N(graph_layers[-1], name='Readout')
         for i, mlp in enumerate(mlp_layers):
             self.mlp_layers.append(layers.Dense(mlp, name='MLP'+str(i), activation=tf.nn.relu))
@@ -112,6 +277,15 @@ class mymodel(tf.keras.Model):
             x = model(x)
         return x
 
+def get_gate_coeff(X1, X2, dim, _b):
+    num_atoms = int(X1.get_shape()[1])
+    _b = tf.reshape(tf.tile(_b, [num_atoms]), [num_atoms, dim])
+    X1 = tf.layers.dense(X1, units=dim, use_bias=False)
+    X2 = tf.layers.dense(X2, units=dim, use_bias=False)
+    output = tf.nn.sigmoid(X1+X2+_b)
+
+    return output
+
 def get_skip_connection(_X, X):
     if( int(_X.get_shape()[2]) != int(X.get_shape()[2]) ):
        out_dim = int(_X.get_shape()[2])
@@ -120,6 +294,20 @@ def get_skip_connection(_X, X):
        _X = _X + X 
 
     return _X 
+
+def attn_matrix(A, X, attn_weight):
+    # A : [batch, N, N]
+    # X : [batch, N, F']
+    # weight_attn : F'
+    num_atoms = int(X.get_shape()[1])
+    hidden_dim = int(X.get_shape()[2])
+
+    _X1 = tf.einsum('ij,ajk->aik', attn_weight, tf.transpose(X, [0,2,1]))
+    _X2 = tf.matmul(X, _X1)
+    _A = tf.multiply(A, _X2)
+    _A = tf.nn.tanh(_A)
+
+    return _A
 
 def load_data(ids, unit=10000):
     """
@@ -135,6 +323,7 @@ def load_data(ids, unit=10000):
         adj = np.concatenate((adj, adj0), axis=0)
     N_sample = len(features)
     prop = np.load('../../augmented-GCN/database/ZINC/logP.npy')[ids[0]*unit:ids[1]*unit]
+    prop = np.reshape(prop, [len(prop), 1])
     return adj, features, prop
 
 class Progress(keras.callbacks.Callback):
@@ -151,16 +340,17 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-m", "--model", dest="model", default='GCN',
                       help="training model, GCN(default)")
-    parser.add_option("-g", "--graph", dest="graph", default='32, 32, 32', #nargs='+', type=int,
+    parser.add_option("-g", "--graph", dest="graph", default='32,32,32', #nargs='+', type=int,
                       help="graph_layers in list, 32, 32, 32(default)")
-    parser.add_option("-l", "--mlp", dest="mlp", default='128, 128', #nargs='+', type=int, 
-                      help="mlp layers in list, 128, 128(default)")
+    parser.add_option("-l", "--mlp", dest="mlp", default='64,64', #nargs='+', type=int, 
+                      help="mlp layers in list, 64,64(default)")
     parser.add_option("-e", "--epoch", dest="epoch", default=100, type=int,
                       help="number of epochs, 200 (default)")
     (options, args) = parser.parse_args()
 
     # Define the adjustable parameters
-    train, test = [0, 1], [1, 2]
+    train, test = [0, 40], [40, 50]
+    #train, test = [0, 2], [2, 3]
     model_name = options.model
     graph_layers = [int(item) for item in options.graph.split(',')]
     mlp_layers = [int(item) for item in options.mlp.split(',')]
@@ -186,6 +376,7 @@ if __name__ == '__main__':
     logging.info('The input model is {:s}'.format(title))
     time0 = time()
     model = mymodel(model_name, graph_layers, mlp_layers)
+
     #optimizer = tf.keras.optimizers.RMSprop(0.001)
     optimizer = tf.keras.optimizers.Adam(lr=lr, decay=1e-6)
     model.compile(loss='mean_squared_error',
@@ -198,9 +389,9 @@ if __name__ == '__main__':
     #                                         write_graph=True, 
     #                                         write_images=True)
 
-    history = model.fit(x=[features1, adj1], y=props1, batch_size=100, 
+    history = model.fit(x=[features1, adj1], y=props1, batch_size=64, 
                         epochs=epoch, verbose=0,
-                        callbacks=[Progress()]
+                        callbacks=[Progress(), keras.callbacks.CSVLogger('training.log')]
                        )
     logging.info('Training is completed in {:.2f} minutes'.format((time()-time0)/60))
     logging.info('Total number of parameters: {:d}'.format(history.model.count_params()))
@@ -208,9 +399,17 @@ if __name__ == '__main__':
     # Analyze the results
     res1 = model.predict([features1,adj1])
     res2 = model.predict([features2,adj2])
+    print(res1.shape, props1.shape)
+    print(r2_score(res1, props1), mean_absolute_error(res1, props1), keras.losses.mae(res1, props1))
+    #print(model.evaluate(x=[features1, adj1], y=props1, batch_size=100))
     logging.info('Train in {:d} samples, r2 {:.4f} mae {:.4f}'.format(len(res1), r2_score(res1, props1), mean_absolute_error(res1, props1)))
     logging.info('Test  in {:d} samples, r2 {:.4f} mae {:.4f}'.format(len(res2), r2_score(res2, props2), mean_absolute_error(res2, props2)))
     
+    #history = model.fit(x=[features1, adj1], y=props1, batch_size=64, 
+    #                    epochs=2, verbose=0,
+    #                    callbacks=[Progress(), keras.callbacks.CSVLogger('training.log')]
+    #                   )
+
     hist = pd.DataFrame(history.history)
     hist['epoch'] = history.epoch
     plt.figure(figsize=(10, 8))
